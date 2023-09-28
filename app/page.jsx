d668c5760c92b52fb90552b3ef0f5a7748935b66
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-09-28 14:26:11 -0300

    Doing basic fetching

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
-export default function Home() {
+async function fetchUsers() {
+  const res = await fetch('https://reqres.in/api/users');
+  const data = await res.json();
+  return data.data;
+}
+
+async function Home() {
+  const users = await fetchUsers();
   return (
     <div>
       <h1>Index Page</h1>
+      <div>{JSON.stringify(users)}</div>
     </div>
   );
 }
+
+export default Home;
