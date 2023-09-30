19b3d99050e45f9699147212856f77492e40c915
Kev-Villegas <kevin.villegas1973@gmail.com>
2023-09-30 11:49:37 -0300

    Add Users Details

--- app/users/[id]/page.jsx
+++ app/users/[id]/page.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const UsersPage = () => {
+async function getUsers(id) {
+  const res = await fetch(`https://reqres.in/api/users/${id}`);
+  const data = await res.json();
+  return data.data;
+}
+async function UsersPage({ params }) {
+  const user = await getUsers(params.id);
   return (
-    <div>User Details...</div>
-  )
+    <div>
+      <h1>Users Details...</h1>
+      <div>
+        <img src={user.avatar} alt='' />
+        <div>
+          <h3>
+            {user.id} {user.first_name} {user.last_name}
+          </h3>
+          <p>{user.email}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default UsersPage
+export default UsersPage;
